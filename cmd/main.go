09e9eec8e6d13dc8b63ec5a8f9e73ce9041f1d41
Владислав Отмахов <vladislav.otmakhov@megamarket.ru>
2023-09-08 11:56:29 +1000

    init

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,7 @@
 package main
 
 import (
-	"context"
 	"net"
-	"os"
-	"os/signal"
 
 	pb "github.com/Neumann88/microservice-1/internal/gen/proto/microservice-1/v1"
 	grpcserver "github.com/Neumann88/microservice-1/internal/grpc/microservice-1/v1"
@@ -12,16 +9,6 @@ import (
 )
 
 func main() {
-	c := make(chan os.Signal, 1)
-	signal.Notify(c, os.Interrupt)
-
-	ctx, cancel := context.WithCancel(context.Background())
-
-	go func() {
-		<-c
-		cancel()
-	}()
-
 	listen, err := net.Listen("tcp", ":5010")
 	if err != nil {
 		panic(err)
@@ -36,11 +23,7 @@ func main() {
 	// 	reflection.Register(s)
 	// }
 
-	go func() {
-		if err := s.Serve(listen); err != nil {
-			panic(err)
-		}
-	}()
-
-	<-ctx.Done()
+	if err := s.Serve(listen); err != nil {
+		panic(err)
+	}
 }
